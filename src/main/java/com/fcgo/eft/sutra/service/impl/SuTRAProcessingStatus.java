8a28788f706e0cfbea8a940809d35940a5544f65
manoj.mahato <kamalmanoj9@gmail.com>
2025-10-13 12:08:31 +0545

    WebClient connection timeout 5 min

--- src/main/java/com/fcgo/eft/sutra/service/impl/SuTRAProcessingStatus.java
+++ src/main/java/com/fcgo/eft/sutra/service/impl/SuTRAProcessingStatus.java
@@ -8,11 +8,12 @@ import com.fcgo.eft.sutra.repository.oracle.EftBatchPaymentDetailRepository;
 import com.fcgo.eft.sutra.repository.oracle.NchlReconciledRepository;
 import com.fcgo.eft.sutra.util.TransactionStatusUpdate;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = CustomException.class)
@@ -22,16 +23,22 @@ public class SuTRAProcessingStatus {
     private final EftBatchPaymentDetailRepository detailRepository;
     private final AccEpaymentRepository epaymentRepository;
     public void check(long eftNo){
-        Optional<NchlReconciled> reconciled = reconciledRepository.findByInstructionId(eftNo);
-        if (reconciled.isPresent()) {
-            statusUpdate.update(reconciled.get());
-        } else {
-            String instructionId = String.valueOf(eftNo);
-            Optional<EftBatchPaymentDetail> batchPaymentDetail =
-                    detailRepository.findByInstructionId(instructionId);
-            if (batchPaymentDetail.isEmpty()) {
-                epaymentRepository.updateRevertInSuTra(eftNo);
+        try {
+            Optional<NchlReconciled> reconciled = reconciledRepository.findByInstructionId(eftNo);
+            if (reconciled.isPresent()) {
+                statusUpdate.update(reconciled.get());
+                log.info("reconciled {}", reconciled.get().getInstructionId());
+            } else {
+                String instructionId = String.valueOf(eftNo);
+                Optional<EftBatchPaymentDetail> batchPaymentDetail =
+                        detailRepository.findByInstructionId(instructionId);
+                if (batchPaymentDetail.isEmpty()) {
+                    epaymentRepository.updateRevertInSuTra(eftNo);
+                    log.info("EFT No. {} has been reverted to sutra", eftNo);
+                } else {
+                    log.info("Transaction found in EftBatchPaymentDetail: {}", instructionId);
+                }
             }
-        }
+        }catch (Exception e){log.info(e.getMessage());}
     }
 }

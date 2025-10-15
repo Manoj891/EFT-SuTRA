edef1cd1fbe0947962ed208f8512a7cdd39cfaba
manoj.mahato <kamalmanoj9@gmail.com>
2025-10-15 14:20:13 +0545

    check Status By Date

--- src/main/java/com/fcgo/eft/sutra/repository/oracle/EftBatchPaymentDetailRepository.java
+++ src/main/java/com/fcgo/eft/sutra/repository/oracle/EftBatchPaymentDetailRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 

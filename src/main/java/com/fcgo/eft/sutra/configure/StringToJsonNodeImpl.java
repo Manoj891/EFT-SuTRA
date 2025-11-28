72245597074c34809861ced6b142c311b0fdba78
manoj.mahato <kamalmanoj9@gmail.com>
2025-11-28 10:40:11 +0545

    date formate

--- src/main/java/com/fcgo/eft/sutra/configure/StringToJsonNodeImpl.java
+++ src/main/java/com/fcgo/eft/sutra/configure/StringToJsonNodeImpl.java
@@ -3,7 +3,6 @@ package com.fcgo.eft.sutra.configure;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +15,10 @@ import java.text.SimpleDateFormat;
 public class StringToJsonNodeImpl implements StringToJsonNode {
     private final ObjectMapper mapper = new ObjectMapper();
     private final JsonFactory factory = mapper.getFactory();
-    @Getter
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    @Getter
     private final SimpleDateFormat yyMMdd = new SimpleDateFormat("yyMMdd");
-    @Getter
     private final SimpleDateFormat yyyyMMddHHmmss = new SimpleDateFormat("yyyyMMddHHmmss");
-    @Override
+
     public JsonNode toJsonNode(String jsonData) {
         try {
             return mapper.readTree(factory.createParser(jsonData));
@@ -32,4 +28,18 @@ public class StringToJsonNodeImpl implements StringToJsonNode {
         return null;
     }
 
+    @Override
+    public SimpleDateFormat getYyyyMMddHHmmss() {
+        return yyyyMMddHHmmss;
+    }
+
+    @Override
+    public SimpleDateFormat getYyMMdd() {
+        return yyMMdd;
+    }
+
+    @Override
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
 }

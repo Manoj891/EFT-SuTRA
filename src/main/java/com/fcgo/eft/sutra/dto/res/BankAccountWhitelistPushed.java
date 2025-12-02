545edde9f3533b7bdc6071f8137000ebc2034b7c
manoj <msware9@gmail.com>
2025-12-02 17:37:36 +0545

    Scheduled fixed

--- src/main/java/com/fcgo/eft/sutra/dto/res/BankAccountWhitelistPushed.java
+++ src/main/java/com/fcgo/eft/sutra/dto/res/BankAccountWhitelistPushed.java
@@ -2,13 +2,6 @@ package com.fcgo.eft.sutra.dto.res;
 
 public interface BankAccountWhitelistPushed {
 
-//    String branchId;
-//   String accountName;
-//  String bankName;
-//  String status;
-//  String rcreTime;
-//String bankId;
-//    String accountId;
 
     String getBranchId();
 
